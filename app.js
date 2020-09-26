41eee09910ff473954f6cb733032cc8fb034d694
mOhsin <mohsinsheikh473@gmail.com>
2020-09-26 16:28:35 +0500

    database integrated

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 let msgLi = document.getElementById("msgli");
 
+let chats = firebase.database().ref("chats"); // Firebase Database Reference
+
 let send = () => {
-    let message = document.getElementById("message");
-    
-    let li = document.createElement('li')
-    li.setAttribute("id","shown")
-    liText = document.createTextNode(message.value);
+  let message = document.getElementById("message");
 
-    li.appendChild(liText);
-    msgLi.appendChild(li);
+  let key = chats.push().key; // Generating Key
+  let msgText = {
+    li: message.value,
+    key: key,
+  };
+  chats.child(key).set(msgText); // Sending Data to Database
+  message.value = " ";
+};
 
+chats.on("child_added", (data) => {
+  // Retreving Data From Database
+  let li = document.createElement("li");
+  li.setAttribute("class", "shown");
+  liText = document.createTextNode(data.val().li);
+  li.appendChild(liText);
+  msgLi.appendChild(li);
+});
+
+let message = document.getElementById("message");
+message.onkeypress = (e) => {
+  // console.log(e)
+  if (e.keyCode === 13) {
+    send();
     message.value = " ";
-}
+  }
+};
